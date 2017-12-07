27f42019173d41587aab681bdfba7f361a0cdea8
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-12-07 15:00:50 -0800

    Align values and temporarily turn of pylints whitespace check around it.

--- adafruit_lsm9ds0.py
+++ adafruit_lsm9ds0.py
@@ -42,73 +42,75 @@ import adafruit_bus_device.spi_device as spi_device
 from micropython import const
 
 # Internal constants and register values:
-_LSM9DS0_ADDRESS_ACCELMAG = const(0x1D)  # 3B >> 1 = 7bit default
-_LSM9DS0_ADDRESS_GYRO = const(0x6B)  # D6 >> 1 = 7bit default
-_LSM9DS0_XM_ID = const(0b01001001)
-_LSM9DS0_G_ID = const(0b11010100)
-_LSM9DS0_ACCEL_MG_LSB_2G = 0.061
-_LSM9DS0_ACCEL_MG_LSB_4G = 0.122
-_LSM9DS0_ACCEL_MG_LSB_6G = 0.183
-_LSM9DS0_ACCEL_MG_LSB_8G = 0.244
-_LSM9DS0_ACCEL_MG_LSB_16G = 0.732 # Is this right? Was expecting 0.488
-_LSM9DS0_MAG_MGAUSS_2GAUSS = 0.08
-_LSM9DS0_MAG_MGAUSS_4GAUSS = 0.16
-_LSM9DS0_MAG_MGAUSS_8GAUSS = 0.32
-_LSM9DS0_MAG_MGAUSS_12GAUSS = 0.48
-_LSM9DS0_GYRO_DPS_DIGIT_245DPS = 0.00875
-_LSM9DS0_GYRO_DPS_DIGIT_500DPS = 0.01750
-_LSM9DS0_GYRO_DPS_DIGIT_2000DPS = 0.07000
-_LSM9DS0_TEMP_LSB_DEGREE_CELSIUS = 8  # 1°C = 8, 25° = 200, etc.
-_LSM9DS0_REGISTER_WHO_AM_I_G = const(0x0F)
-_LSM9DS0_REGISTER_CTRL_REG1_G = const(0x20)
-_LSM9DS0_REGISTER_CTRL_REG3_G = const(0x22)
-_LSM9DS0_REGISTER_CTRL_REG4_G = const(0x23)
-_LSM9DS0_REGISTER_OUT_X_L_G = const(0x28)
-_LSM9DS0_REGISTER_OUT_X_H_G = const(0x29)
-_LSM9DS0_REGISTER_OUT_Y_L_G = const(0x2A)
-_LSM9DS0_REGISTER_OUT_Y_H_G = const(0x2B)
-_LSM9DS0_REGISTER_OUT_Z_L_G = const(0x2C)
-_LSM9DS0_REGISTER_OUT_Z_H_G = const(0x2D)
-_LSM9DS0_REGISTER_TEMP_OUT_L_XM = const(0x05)
-_LSM9DS0_REGISTER_TEMP_OUT_H_XM = const(0x06)
-_LSM9DS0_REGISTER_STATUS_REG_M = const(0x07)
-_LSM9DS0_REGISTER_OUT_X_L_M = const(0x08)
-_LSM9DS0_REGISTER_OUT_X_H_M = const(0x09)
-_LSM9DS0_REGISTER_OUT_Y_L_M = const(0x0A)
-_LSM9DS0_REGISTER_OUT_Y_H_M = const(0x0B)
-_LSM9DS0_REGISTER_OUT_Z_L_M = const(0x0C)
-_LSM9DS0_REGISTER_OUT_Z_H_M = const(0x0D)
-_LSM9DS0_REGISTER_WHO_AM_I_XM = const(0x0F)
-_LSM9DS0_REGISTER_INT_CTRL_REG_M = const(0x12)
-_LSM9DS0_REGISTER_INT_SRC_REG_M = const(0x13)
-_LSM9DS0_REGISTER_CTRL_REG1_XM = const(0x20)
-_LSM9DS0_REGISTER_CTRL_REG2_XM = const(0x21)
-_LSM9DS0_REGISTER_CTRL_REG5_XM = const(0x24)
-_LSM9DS0_REGISTER_CTRL_REG6_XM = const(0x25)
-_LSM9DS0_REGISTER_CTRL_REG7_XM = const(0x26)
-_LSM9DS0_REGISTER_OUT_X_L_A = const(0x28)
-_LSM9DS0_REGISTER_OUT_X_H_A = const(0x29)
-_LSM9DS0_REGISTER_OUT_Y_L_A = const(0x2A)
-_LSM9DS0_REGISTER_OUT_Y_H_A = const(0x2B)
-_LSM9DS0_REGISTER_OUT_Z_L_A = const(0x2C)
-_LSM9DS0_REGISTER_OUT_Z_H_A = const(0x2D)
-_GYROTYPE = True
-_XMTYPE = False
-_SENSORS_GRAVITY_STANDARD = 9.80665
+# pylint: disable=bad-whitespace
+_LSM9DS0_ADDRESS_ACCELMAG           = const(0x1D)  # 3B >> 1 = 7bit default
+_LSM9DS0_ADDRESS_GYRO               = const(0x6B)  # D6 >> 1 = 7bit default
+_LSM9DS0_XM_ID                      = const(0b01001001)
+_LSM9DS0_G_ID                       = const(0b11010100)
+_LSM9DS0_ACCEL_MG_LSB_2G            = 0.061
+_LSM9DS0_ACCEL_MG_LSB_4G            = 0.122
+_LSM9DS0_ACCEL_MG_LSB_6G            = 0.183
+_LSM9DS0_ACCEL_MG_LSB_8G            = 0.244
+_LSM9DS0_ACCEL_MG_LSB_16G           = 0.732 # Is this right? Was expecting 0.488
+_LSM9DS0_MAG_MGAUSS_2GAUSS          = 0.08
+_LSM9DS0_MAG_MGAUSS_4GAUSS          = 0.16
+_LSM9DS0_MAG_MGAUSS_8GAUSS          = 0.32
+_LSM9DS0_MAG_MGAUSS_12GAUSS         = 0.48
+_LSM9DS0_GYRO_DPS_DIGIT_245DPS      = 0.00875
+_LSM9DS0_GYRO_DPS_DIGIT_500DPS      = 0.01750
+_LSM9DS0_GYRO_DPS_DIGIT_2000DPS     = 0.07000
+_LSM9DS0_TEMP_LSB_DEGREE_CELSIUS    = 8  # 1°C = 8, 25° = 200, etc.
+_LSM9DS0_REGISTER_WHO_AM_I_G        = const(0x0F)
+_LSM9DS0_REGISTER_CTRL_REG1_G       = const(0x20)
+_LSM9DS0_REGISTER_CTRL_REG3_G       = const(0x22)
+_LSM9DS0_REGISTER_CTRL_REG4_G       = const(0x23)
+_LSM9DS0_REGISTER_OUT_X_L_G         = const(0x28)
+_LSM9DS0_REGISTER_OUT_X_H_G         = const(0x29)
+_LSM9DS0_REGISTER_OUT_Y_L_G         = const(0x2A)
+_LSM9DS0_REGISTER_OUT_Y_H_G         = const(0x2B)
+_LSM9DS0_REGISTER_OUT_Z_L_G         = const(0x2C)
+_LSM9DS0_REGISTER_OUT_Z_H_G         = const(0x2D)
+_LSM9DS0_REGISTER_TEMP_OUT_L_XM     = const(0x05)
+_LSM9DS0_REGISTER_TEMP_OUT_H_XM     = const(0x06)
+_LSM9DS0_REGISTER_STATUS_REG_M      = const(0x07)
+_LSM9DS0_REGISTER_OUT_X_L_M         = const(0x08)
+_LSM9DS0_REGISTER_OUT_X_H_M         = const(0x09)
+_LSM9DS0_REGISTER_OUT_Y_L_M         = const(0x0A)
+_LSM9DS0_REGISTER_OUT_Y_H_M         = const(0x0B)
+_LSM9DS0_REGISTER_OUT_Z_L_M         = const(0x0C)
+_LSM9DS0_REGISTER_OUT_Z_H_M         = const(0x0D)
+_LSM9DS0_REGISTER_WHO_AM_I_XM       = const(0x0F)
+_LSM9DS0_REGISTER_INT_CTRL_REG_M    = const(0x12)
+_LSM9DS0_REGISTER_INT_SRC_REG_M     = const(0x13)
+_LSM9DS0_REGISTER_CTRL_REG1_XM      = const(0x20)
+_LSM9DS0_REGISTER_CTRL_REG2_XM      = const(0x21)
+_LSM9DS0_REGISTER_CTRL_REG5_XM      = const(0x24)
+_LSM9DS0_REGISTER_CTRL_REG6_XM      = const(0x25)
+_LSM9DS0_REGISTER_CTRL_REG7_XM      = const(0x26)
+_LSM9DS0_REGISTER_OUT_X_L_A         = const(0x28)
+_LSM9DS0_REGISTER_OUT_X_H_A         = const(0x29)
+_LSM9DS0_REGISTER_OUT_Y_L_A         = const(0x2A)
+_LSM9DS0_REGISTER_OUT_Y_H_A         = const(0x2B)
+_LSM9DS0_REGISTER_OUT_Z_L_A         = const(0x2C)
+_LSM9DS0_REGISTER_OUT_Z_H_A         = const(0x2D)
+_GYROTYPE                           = True
+_XMTYPE                             = False
+_SENSORS_GRAVITY_STANDARD           = 9.80665
 
 # User facing constants/module globals.
-ACCELRANGE_2G = (0b000 << 3)
-ACCELRANGE_4G = (0b001 << 3)
-ACCELRANGE_6G = (0b010 << 3)
-ACCELRANGE_8G = (0b011 << 3)
-ACCELRANGE_16G = (0b100 << 3)
-MAGGAIN_2GAUSS = (0b00 << 5)     # +/- 2 gauss
-MAGGAIN_4GAUSS = (0b01 << 5)     # +/- 4 gauss
-MAGGAIN_8GAUSS = (0b10 << 5)     # +/- 8 gauss
-MAGGAIN_12GAUSS = (0b11 << 5)    # +/- 12 gauss
-GYROSCALE_245DPS = (0b00 << 4)   # +/- 245 degrees per second rotation
-GYROSCALE_500DPS = (0b01 << 4)   # +/- 500 degrees per second rotation
-GYROSCALE_2000DPS = (0b10 << 4)  # +/- 2000 degrees per second rotation
+ACCELRANGE_2G                = (0b000 << 3)
+ACCELRANGE_4G                = (0b001 << 3)
+ACCELRANGE_6G                = (0b010 << 3)
+ACCELRANGE_8G                = (0b011 << 3)
+ACCELRANGE_16G               = (0b100 << 3)
+MAGGAIN_2GAUSS               = (0b00 << 5)  # +/- 2 gauss
+MAGGAIN_4GAUSS               = (0b01 << 5)  # +/- 4 gauss
+MAGGAIN_8GAUSS               = (0b10 << 5)  # +/- 8 gauss
+MAGGAIN_12GAUSS              = (0b11 << 5)  # +/- 12 gauss
+GYROSCALE_245DPS             = (0b00 << 4)  # +/- 245 degrees per second rotation
+GYROSCALE_500DPS             = (0b01 << 4)  # +/- 500 degrees per second rotation
+GYROSCALE_2000DPS            = (0b10 << 4)  # +/- 2000 degrees per second rotation
+# pylint: enable=bad-whitespace
 
 
 def _twos_comp(val, bits):
