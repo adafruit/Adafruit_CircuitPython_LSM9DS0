55e83de46cc816f32577988661b5254ecea37fb4
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-12-07 13:06:08 -0800

    Update to new build process and turn on lint.

--- adafruit_lsm9ds0.py
+++ adafruit_lsm9ds0.py
@@ -31,81 +31,84 @@ See examples/simpletest.py for a demo of the usage.
 
 * Author(s): Tony DiCola
 """
-import time
-import ustruct
+try:
+    import struct
+except ImportError:
+    import ustruct as struct
 
 import adafruit_bus_device.i2c_device as i2c_device
 import adafruit_bus_device.spi_device as spi_device
 
+from micropython import const
 
 # Internal constants and register values:
-_LSM9DS0_ADDRESS_ACCELMAG           = const(0x1D)  # 3B >> 1 = 7bit default
-_LSM9DS0_ADDRESS_GYRO               = const(0x6B)  # D6 >> 1 = 7bit default
-_LSM9DS0_XM_ID                      = const(0b01001001)
-_LSM9DS0_G_ID                       = const(0b11010100)
-_LSM9DS0_ACCEL_MG_LSB_2G            = 0.061
-_LSM9DS0_ACCEL_MG_LSB_4G            = 0.122
-_LSM9DS0_ACCEL_MG_LSB_6G            = 0.183
-_LSM9DS0_ACCEL_MG_LSB_8G            = 0.244
-_LSM9DS0_ACCEL_MG_LSB_16G           = 0.732 # Is this right? Was expecting 0.488
-_LSM9DS0_MAG_MGAUSS_2GAUSS          = 0.08
-_LSM9DS0_MAG_MGAUSS_4GAUSS          = 0.16
-_LSM9DS0_MAG_MGAUSS_8GAUSS          = 0.32
-_LSM9DS0_MAG_MGAUSS_12GAUSS         = 0.48
-_LSM9DS0_GYRO_DPS_DIGIT_245DPS      = 0.00875
-_LSM9DS0_GYRO_DPS_DIGIT_500DPS      = 0.01750
-_LSM9DS0_GYRO_DPS_DIGIT_2000DPS     = 0.07000
-_LSM9DS0_TEMP_LSB_DEGREE_CELSIUS    = 8  # 1°C = 8, 25° = 200, etc.
-_LSM9DS0_REGISTER_WHO_AM_I_G        = const(0x0F)
-_LSM9DS0_REGISTER_CTRL_REG1_G       = const(0x20)
-_LSM9DS0_REGISTER_CTRL_REG3_G       = const(0x22)
-_LSM9DS0_REGISTER_CTRL_REG4_G       = const(0x23)
-_LSM9DS0_REGISTER_OUT_X_L_G         = const(0x28)
-_LSM9DS0_REGISTER_OUT_X_H_G         = const(0x29)
-_LSM9DS0_REGISTER_OUT_Y_L_G         = const(0x2A)
-_LSM9DS0_REGISTER_OUT_Y_H_G         = const(0x2B)
-_LSM9DS0_REGISTER_OUT_Z_L_G         = const(0x2C)
-_LSM9DS0_REGISTER_OUT_Z_H_G         = const(0x2D)
-_LSM9DS0_REGISTER_TEMP_OUT_L_XM     = const(0x05)
-_LSM9DS0_REGISTER_TEMP_OUT_H_XM     = const(0x06)
-_LSM9DS0_REGISTER_STATUS_REG_M      = const(0x07)
-_LSM9DS0_REGISTER_OUT_X_L_M         = const(0x08)
-_LSM9DS0_REGISTER_OUT_X_H_M         = const(0x09)
-_LSM9DS0_REGISTER_OUT_Y_L_M         = const(0x0A)
-_LSM9DS0_REGISTER_OUT_Y_H_M         = const(0x0B)
-_LSM9DS0_REGISTER_OUT_Z_L_M         = const(0x0C)
-_LSM9DS0_REGISTER_OUT_Z_H_M         = const(0x0D)
-_LSM9DS0_REGISTER_WHO_AM_I_XM       = const(0x0F)
-_LSM9DS0_REGISTER_INT_CTRL_REG_M    = const(0x12)
-_LSM9DS0_REGISTER_INT_SRC_REG_M     = const(0x13)
-_LSM9DS0_REGISTER_CTRL_REG1_XM      = const(0x20)
-_LSM9DS0_REGISTER_CTRL_REG2_XM      = const(0x21)
-_LSM9DS0_REGISTER_CTRL_REG5_XM      = const(0x24)
-_LSM9DS0_REGISTER_CTRL_REG6_XM      = const(0x25)
-_LSM9DS0_REGISTER_CTRL_REG7_XM      = const(0x26)
-_LSM9DS0_REGISTER_OUT_X_L_A         = const(0x28)
-_LSM9DS0_REGISTER_OUT_X_H_A         = const(0x29)
-_LSM9DS0_REGISTER_OUT_Y_L_A         = const(0x2A)
-_LSM9DS0_REGISTER_OUT_Y_H_A         = const(0x2B)
-_LSM9DS0_REGISTER_OUT_Z_L_A         = const(0x2C)
-_LSM9DS0_REGISTER_OUT_Z_H_A         = const(0x2D)
-_GYROTYPE                           = True
-_XMTYPE                             = False
-_SENSORS_GRAVITY_STANDARD           = 9.80665
+_LSM9DS0_ADDRESS_ACCELMAG = const(0x1D)  # 3B >> 1 = 7bit default
+_LSM9DS0_ADDRESS_GYRO = const(0x6B)  # D6 >> 1 = 7bit default
+_LSM9DS0_XM_ID = const(0b01001001)
+_LSM9DS0_G_ID = const(0b11010100)
+_LSM9DS0_ACCEL_MG_LSB_2G = 0.061
+_LSM9DS0_ACCEL_MG_LSB_4G = 0.122
+_LSM9DS0_ACCEL_MG_LSB_6G = 0.183
+_LSM9DS0_ACCEL_MG_LSB_8G = 0.244
+_LSM9DS0_ACCEL_MG_LSB_16G = 0.732 # Is this right? Was expecting 0.488
+_LSM9DS0_MAG_MGAUSS_2GAUSS = 0.08
+_LSM9DS0_MAG_MGAUSS_4GAUSS = 0.16
+_LSM9DS0_MAG_MGAUSS_8GAUSS = 0.32
+_LSM9DS0_MAG_MGAUSS_12GAUSS = 0.48
+_LSM9DS0_GYRO_DPS_DIGIT_245DPS = 0.00875
+_LSM9DS0_GYRO_DPS_DIGIT_500DPS = 0.01750
+_LSM9DS0_GYRO_DPS_DIGIT_2000DPS = 0.07000
+_LSM9DS0_TEMP_LSB_DEGREE_CELSIUS = 8  # 1°C = 8, 25° = 200, etc.
+_LSM9DS0_REGISTER_WHO_AM_I_G = const(0x0F)
+_LSM9DS0_REGISTER_CTRL_REG1_G = const(0x20)
+_LSM9DS0_REGISTER_CTRL_REG3_G = const(0x22)
+_LSM9DS0_REGISTER_CTRL_REG4_G = const(0x23)
+_LSM9DS0_REGISTER_OUT_X_L_G = const(0x28)
+_LSM9DS0_REGISTER_OUT_X_H_G = const(0x29)
+_LSM9DS0_REGISTER_OUT_Y_L_G = const(0x2A)
+_LSM9DS0_REGISTER_OUT_Y_H_G = const(0x2B)
+_LSM9DS0_REGISTER_OUT_Z_L_G = const(0x2C)
+_LSM9DS0_REGISTER_OUT_Z_H_G = const(0x2D)
+_LSM9DS0_REGISTER_TEMP_OUT_L_XM = const(0x05)
+_LSM9DS0_REGISTER_TEMP_OUT_H_XM = const(0x06)
+_LSM9DS0_REGISTER_STATUS_REG_M = const(0x07)
+_LSM9DS0_REGISTER_OUT_X_L_M = const(0x08)
+_LSM9DS0_REGISTER_OUT_X_H_M = const(0x09)
+_LSM9DS0_REGISTER_OUT_Y_L_M = const(0x0A)
+_LSM9DS0_REGISTER_OUT_Y_H_M = const(0x0B)
+_LSM9DS0_REGISTER_OUT_Z_L_M = const(0x0C)
+_LSM9DS0_REGISTER_OUT_Z_H_M = const(0x0D)
+_LSM9DS0_REGISTER_WHO_AM_I_XM = const(0x0F)
+_LSM9DS0_REGISTER_INT_CTRL_REG_M = const(0x12)
+_LSM9DS0_REGISTER_INT_SRC_REG_M = const(0x13)
+_LSM9DS0_REGISTER_CTRL_REG1_XM = const(0x20)
+_LSM9DS0_REGISTER_CTRL_REG2_XM = const(0x21)
+_LSM9DS0_REGISTER_CTRL_REG5_XM = const(0x24)
+_LSM9DS0_REGISTER_CTRL_REG6_XM = const(0x25)
+_LSM9DS0_REGISTER_CTRL_REG7_XM = const(0x26)
+_LSM9DS0_REGISTER_OUT_X_L_A = const(0x28)
+_LSM9DS0_REGISTER_OUT_X_H_A = const(0x29)
+_LSM9DS0_REGISTER_OUT_Y_L_A = const(0x2A)
+_LSM9DS0_REGISTER_OUT_Y_H_A = const(0x2B)
+_LSM9DS0_REGISTER_OUT_Z_L_A = const(0x2C)
+_LSM9DS0_REGISTER_OUT_Z_H_A = const(0x2D)
+_GYROTYPE = True
+_XMTYPE = False
+_SENSORS_GRAVITY_STANDARD = 9.80665
 
 # User facing constants/module globals.
-ACCELRANGE_2G                = (0b000 << 3)
-ACCELRANGE_4G                = (0b001 << 3)
-ACCELRANGE_6G                = (0b010 << 3)
-ACCELRANGE_8G                = (0b011 << 3)
-ACCELRANGE_16G               = (0b100 << 3)
-MAGGAIN_2GAUSS               = (0b00 << 5)  # +/- 2 gauss
-MAGGAIN_4GAUSS               = (0b01 << 5)  # +/- 4 gauss
-MAGGAIN_8GAUSS               = (0b10 << 5)  # +/- 8 gauss
-MAGGAIN_12GAUSS              = (0b11 << 5)  # +/- 12 gauss
-GYROSCALE_245DPS             = (0b00 << 4)  # +/- 245 degrees per second rotation
-GYROSCALE_500DPS             = (0b01 << 4)  # +/- 500 degrees per second rotation
-GYROSCALE_2000DPS            = (0b10 << 4)  # +/- 2000 degrees per second rotation
+ACCELRANGE_2G = (0b000 << 3)
+ACCELRANGE_4G = (0b001 << 3)
+ACCELRANGE_6G = (0b010 << 3)
+ACCELRANGE_8G = (0b011 << 3)
+ACCELRANGE_16G = (0b100 << 3)
+MAGGAIN_2GAUSS = (0b00 << 5)     # +/- 2 gauss
+MAGGAIN_4GAUSS = (0b01 << 5)     # +/- 4 gauss
+MAGGAIN_8GAUSS = (0b10 << 5)     # +/- 8 gauss
+MAGGAIN_12GAUSS = (0b11 << 5)    # +/- 12 gauss
+GYROSCALE_245DPS = (0b00 << 4)   # +/- 245 degrees per second rotation
+GYROSCALE_500DPS = (0b01 << 4)   # +/- 500 degrees per second rotation
+GYROSCALE_2000DPS = (0b10 << 4)  # +/- 2000 degrees per second rotation
 
 
 def _twos_comp(val, bits):
@@ -113,12 +116,11 @@ def _twos_comp(val, bits):
     # length to its signed integer value and return it.
     if val & (1 << (bits - 1)) != 0:
         return val - (1 << bits)
-    else:
-        return val
+    return val
 
 
 class LSM9DS0:
-
+    """Driver for the LSM9DS0 accelerometer, magnetometer, gyroscope."""
     # Class-level buffer for reading and writing data with the sensor.
     # This reduces memory allocations but means the code is not re-entrant or
     # thread safe!
@@ -137,9 +139,12 @@ class LSM9DS0:
         # enable gyro continuous
         self._write_u8(_GYROTYPE, _LSM9DS0_REGISTER_CTRL_REG1_G, 0x0F)
         # enable the temperature sensor (output rate same as the mag sensor)
-        tempReg = self._read_u8(_XMTYPE, _LSM9DS0_REGISTER_CTRL_REG5_XM)
-        self._write_u8(_XMTYPE, _LSM9DS0_REGISTER_CTRL_REG5_XM, tempReg | (1<<7))
+        temp_reg = self._read_u8(_XMTYPE, _LSM9DS0_REGISTER_CTRL_REG5_XM)
+        self._write_u8(_XMTYPE, _LSM9DS0_REGISTER_CTRL_REG5_XM, temp_reg | (1<<7))
         # Set default ranges for the various sensors
+        self._accel_mg_lsb = None
+        self._mag_mgauss_lsb = None
+        self._gyro_dps_digit = None
         self.accel_range = ACCELRANGE_2G
         self.mag_gain = MAGGAIN_2GAUSS
         self.gyro_scale = GYROSCALE_245DPS
@@ -236,9 +241,7 @@ class LSM9DS0:
         # Read the accelerometer
         self._read_bytes(_XMTYPE, 0x80 | _LSM9DS0_REGISTER_OUT_X_L_A, 6,
                          self._BUFFER)
-        raw_x = ustruct.unpack_from('<h', self._BUFFER[0:2])[0]
-        raw_y = ustruct.unpack_from('<h', self._BUFFER[2:4])[0]
-        raw_z = ustruct.unpack_from('<h', self._BUFFER[4:6])[0]
+        raw_x, raw_y, raw_z = struct.unpack_from('<hhh', self._BUFFER[0:6])
         return (raw_x, raw_y, raw_z)
 
     @property
@@ -259,9 +262,7 @@ class LSM9DS0:
         # Read the magnetometer
         self._read_bytes(_XMTYPE, 0x80 | _LSM9DS0_REGISTER_OUT_X_L_M, 6,
                          self._BUFFER)
-        raw_x = ustruct.unpack_from('<h', self._BUFFER[0:2])[0]
-        raw_y = ustruct.unpack_from('<h', self._BUFFER[2:4])[0]
-        raw_z = ustruct.unpack_from('<h', self._BUFFER[4:6])[0]
+        raw_x, raw_y, raw_z = struct.unpack_from('<hhh', self._BUFFER[0:6])
         return (raw_x, raw_y, raw_z)
 
     @property
@@ -281,9 +282,7 @@ class LSM9DS0:
         # Read the gyroscope
         self._read_bytes(_GYROTYPE, 0x80 | _LSM9DS0_REGISTER_OUT_X_L_G, 6,
                          self._BUFFER)
-        raw_x = ustruct.unpack_from('<h', self._BUFFER[0:2])[0]
-        raw_y = ustruct.unpack_from('<h', self._BUFFER[2:4])[0]
-        raw_z = ustruct.unpack_from('<h', self._BUFFER[4:6])[0]
+        raw_x, raw_y, raw_z = struct.unpack_from('<hhh', self._BUFFER[0:6])
         return (raw_x, raw_y, raw_z)
 
     @property
@@ -320,7 +319,7 @@ class LSM9DS0:
         # MUST be implemented by subclasses!
         raise NotImplementedError()
 
-    def _read_bytes(self, sensor_type, address, count, buffer):
+    def _read_bytes(self, sensor_type, address, count, buf):
         # Read a count number of bytes into buffer from the provided 8-bit
         # register address.  The sensor_type boolean should be _MAGTYPE when
         # talking to the magnetometer, or _XGTYPE when talking to the accel or
@@ -336,10 +335,11 @@ class LSM9DS0:
 
 
 class LSM9DS0_I2C(LSM9DS0):
+    """Driver for the LSM9DS0 connected over I2C."""
 
     def __init__(self, i2c):
         self._gyro_device = i2c_device.I2CDevice(i2c, _LSM9DS0_ADDRESS_GYRO)
-        self._xm_device   = i2c_device.I2CDevice(i2c, _LSM9DS0_ADDRESS_ACCELMAG)
+        self._xm_device = i2c_device.I2CDevice(i2c, _LSM9DS0_ADDRESS_ACCELMAG)
         super().__init__()
 
     def _read_u8(self, sensor_type, address):
@@ -375,10 +375,11 @@ class LSM9DS0_I2C(LSM9DS0):
 
 
 class LSM9DS0_SPI(LSM9DS0):
+    """Driver for the LSM9DS0 connected over SPI."""
 
     def __init__(self, spi, xmcs, gcs):
         self._gyro_device = spi_device.I2CDevice(spi, gcs)
-        self._xm_device   = spi_device.I2CDevice(spi, xmcs)
+        self._xm_device = spi_device.I2CDevice(spi, xmcs)
         super().__init__()
 
     def _read_u8(self, sensor_type, address):
