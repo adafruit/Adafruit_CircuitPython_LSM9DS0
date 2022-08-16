44a72a46bb4b5471f9e2bc34bf0c34d8e447faac
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_lsm9ds0.py
+++ adafruit_lsm9ds0.py
@@ -39,7 +39,7 @@ from digitalio import Direction
 
 from micropython import const
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_LSM9DS0.git"
 
 # Internal constants and register values:
